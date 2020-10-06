4dd01efd422c08fd52c62c9d41ff7cbf968988c4
fati-ma <fatima.atiyya@yahoo.com>
2020-10-06 15:30:37 +0300

    AboutMe Oct6th

--- README.md
+++ README.md
@@ -1,10 +1,14 @@
-# Lab: 02 
+# Lab: 03
 
-## About Me, with a guessing game
+## Extend the “About Me” project
 
 ### I build an app so people can know me better using the "Guessing Game". It asks the user to answer some questions about me so she or he can know how much they know me. Using html/ css/ JS, it first asks by using prompt then displays if the answer is correct or wrong, after that the webpage will be displyed with a bio and the correct answers for the quesions.
 
+### Then the user will get text-answer questions and she/he has to answer them, after that an alert will be displayed to tell them if they answered correctly or not.
+
+### In the end the user will get a score which is the number of correct answers.
+
+### The HTML page will include the answers of the questions, a biography and a top 10 page.
+
 ### Author: Fatima Atieh
 
-### Links and Resources
-#### - View the rendered webpage from [here]().
--- js/app.js
+++ js/app.js
@@ -1,9 +1,11 @@
-'use strict'
+'use strict';
 
 alert('Hello there! You will play a tiny fun game to see how much you know me!');
-alert('Are you ready?');
+alert('The game has 2 rounds. Are you ready?');
 
-var userName= prompt('First, let me know your name :)');
+var score = 0;
+
+var userName = prompt('First, let me know your name :)');
 console.log(userName);
 
 alert(`Ya welcome ya welcome ${userName}! You can only answer using y/n or Yes/No, deal?`);
@@ -12,59 +14,68 @@ var favColor = prompt(`So ${userName}, Is purple my favorite color?`);
 console.log(favColor);
 favColor.toLowerCase();
 console.log(favColor);
-if (favColor === 'y' || favColor === 'yes'){
+if (favColor === 'y' || favColor === 'yes') {
     alert('Correct! Fatima loves purple and purple loves Fatima<3');
-} else if(favColor === 'n' || favColor === 'no'){
+    score++;
+} else if (favColor === 'n' || favColor === 'no') {
     alert('Opps! that was not correct, actually purple is my fav color.')
-} else{
+} else {
     alert('You only can answer using y/n, yes/no. Now you will be taken to the next question');
 }
+console.log(score);
 
 var favField = prompt(`Now for the second question ${userName}, Is Machine Learning my (study) area of interest?`);
 console.log(favField);
-favColor.toLowerCase();
+favField.toLowerCase();
 console.log(favField);
-if (favField === 'y' || favField === 'yes'){
+if (favField === 'y' || favField === 'yes') {
     alert('Yay! That was correct :) NLP is the filed of ML I work on');
-} else if(favField === 'n' || favField === 'no'){
+    score++;
+} else if (favField === 'n' || favField === 'no') {
     alert('Uh-Oh, it\'s ok that was a hard question')
-} else{
+} else {
     alert('You only can answer using y/n, yes/no. Now you will be taken to the next question');
 }
+console.log(score);
 
 var sport = prompt(`The third question is coming ${userName}, So, do I play Karate?`);
 console.log(sport);
-favColor.toLowerCase();
+sport.toLowerCase();
 console.log(sport);
-if (sport === 'y' || sport === 'yes'){
+if (sport === 'y' || sport === 'yes') {
     alert(`I really appreciate you answred this correctly lol! and yes I used to play Karate and got the black belt.`);
-} else if(sport === 'n' || sport === 'no'){
+    score++;
+} else if (sport === 'n' || sport === 'no') {
     alert('Maybe if we had a match you would answer the question right.')
-} else{
+} else {
     alert('You only can answer using y/n, yes/no. Now you will be taken to the next question');
 }
+console.log(score);
 
 var food = prompt(`Forth question ${userName}, Is Mansaf the only dish I can cook?`);
 console.log(food);
-favColor.toLowerCase();
+food.toLowerCase();
 console.log(food);
-if (food === 'y' || food === 'yes'){
+if (food === 'y' || food === 'yes') {
     alert('I can already smell the Jameed! Good job that was correct! wish me good luck in cooking the rest of food :(');
-} else if(food === 'n' || food === 'no'){
+    score++;
+} else if (food === 'n' || food === 'no') {
     alert('Did you just answered with "NO" to Mansaf?!');
-} else{
+} else {
     alert('You only can answer using y/n, yes/no. Now you will be taken to the next question');
 }
+console.log(score);
 
-var asac = prompt(`Final question ${userName}, I'm happy with ASAC?`);
+var asac = prompt(`Final question of the first round ${userName}, I'm happy with ASAC?`);
 console.log(asac);
-favColor.toLowerCase();
+asac.toLowerCase();
 console.log(asac);
 
-switch(asac){
-    case 'y': 
+switch (asac) {
+    case 'y':
     case 'yes':
         alert('Imagin if MY answer is no lol. That was correct! how did you know that? Ehm Ehmm.');
+        score++;
         break;
     case 'n':
     case 'no':
@@ -72,7 +83,45 @@ switch(asac){
         break;
     default:
         alert('You only can answer using y/n, yes/no. Now you will be taken to the next question');
-
 }
-alert(`Thank you ${userName} for playing the game!`);
+console.log(score);
+
+alert('Now for the second round! Ready?')
+
+var myAge;
+for (var ii=0; ii<4; ii++){
+    myAge = prompt('Can you guess my age?');
+    if (myAge == 22) {
+        alert('Correct!');
+        score++;
+        break;}
+    else if (myAge <= 21) {
+        alert('Ops! too low.');
+    }else if (myAge >= 23) {
+        alert('Uh-oh! too high!');  
+    }else {
+        alert('Try again')
+    }
+}
+console.log(myAge);
+alert('My age is 22');
+console.log(score);
+
+
+var mySongs = ['mountain', 'believer', 'worriors', 'fighter'];
+var favSong;
+for (var i = 0; i < 6; i++) {
+        favSong = prompt('Can you guess one of my favorite songs?');
+        if (favSong == 'mountain' || favSong == 'believer' || favSong == 'worriors' || favSong == 'fighter') {
+            alert('Correct!');
+            score++;
+            break;
+        } else {
+            alert('Try again');
+        }
+    }
+console.log(favSong);    
+alert(`My favorite songs are ${mySongs}`);
+
+alert(`Thank you ${userName} for playing the game! Your score is ${score} out of 7`);
 
